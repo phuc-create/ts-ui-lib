01113824bab8dd4d50dd1e4a53f8ae43910922da
phuc.nguyen <phuc.nguyen@phucnguyens-Mac-mini.local>
2022-02-21 15:47:37 +0700

    fix: syntax highlight for ts environment

--- ui-web-page/src/app/usage/CodeHighlight.tsx
+++ ui-web-page/src/app/usage/CodeHighlight.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 interface CodeHighlightProps {
   children: string
